48f37c1ca1a868574ae80627946921e620f69eea
manbhagss <30951328+manbhagss@users.noreply.github.com>
2018-05-17 00:53:21 +0100

    Update genswapfile.sh

--- genswapfile.sh
+++ genswapfile.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 #### The swap file creator : run this as su user ####
-su
 install -o root -g root -m 0600 /dev/null /swapfile
 dd if=/dev/zero of=/swapfile bs=1k count=2048k
 mkswap /swapfile
